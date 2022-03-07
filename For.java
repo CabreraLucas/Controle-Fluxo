cbcc72e1e553006fad1d7a276c1a4ebeb47c0723
CabreraLucas <lucas.cabrera@fatec.sp.gov.br>
2022-03-07 19:49:42 +0000

    ForArray

--- For.java
+++ For.java
@@ -1,7 +1,7 @@
 public class For {
     public static void main (String[] args){
         for(int x = 0; x<1000; x++){
-            System.out.println("Valor: "+x);
+            System.out.print("Valor: "+x);
         }
     }
     
